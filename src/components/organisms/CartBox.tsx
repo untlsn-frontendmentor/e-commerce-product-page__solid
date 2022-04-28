f3a024dde9ff657b83e58833a9c836d917bde85d
Filip Skoczeń <philipskoczen@gmail.com>
2022-04-28 17:09:54 +0200

    Fix empty card

--- src/components/organisms/CartBox.tsx
+++ src/components/organisms/CartBox.tsx
@@ -20,34 +20,42 @@ function CartBox() {
       >
         <h1 class="py-5 pl-8 font-bold">Cart</h1>
         <hr class="border-gray-200"/>
-        <ul class="p-8 pb-0 space-y-6">
-          <For each={cartStore.products}>{(product, i) => (
-            <li class="flex justify-between items-center gap-4">
-              <img class="h-14 rounded-lg" src={product.image} alt="product"/>
-              <div class="text-black/50 flex-1">
-                <h1>{product.name}</h1>
-                <p>
-                  <span>${product.price} x {product.count} </span>
-                  <b class="text-black">
-                    ${product.price * product.count}
-                  </b>
-                </p>
-              </div>
-              <button onClick={() => {
-                setCartStore('products', produce((products: Product[]) => {
-                  products.splice(i(), 1);
-                }));
-              }}>
-                <Trash/>
-              </button>
-            </li>
-          )}</For>
-        </ul>
-        <div class="p-6">
-          <Button>
-            Checkout
-          </Button>
-        </div>
+        <Show
+          when={cartStore.products.length}
+          fallback={(
+            <p class="font-bold opacity-50 text-center py-24">Your cart is empty.</p>
+          )}
+        >
+          <ul class="p-8 pb-0 space-y-6">
+            <For each={cartStore.products}>{(product, i) => (
+              <li class="flex justify-between items-center gap-4">
+                <img class="h-14 rounded-lg" src={product.image} alt="product"/>
+                <div class="text-black/50 flex-1">
+                  <h1>{product.name}</h1>
+                  <p>
+                    <span>${product.price} x {product.count} </span>
+                    <b class="text-black">
+                      ${product.price * product.count}
+                    </b>
+                  </p>
+                </div>
+                <button onClick={() => {
+                  setCartStore('products', produce((products: Product[]) => {
+                    products.splice(i(), 1);
+                  }));
+                }}>
+                  <Trash/>
+                </button>
+              </li>
+            )}</For>
+          </ul>
+          <div class="p-6">
+            <Button>
+              Checkout
+            </Button>
+          </div>
+        </Show>
+
       </article>
     </Show>
   );
